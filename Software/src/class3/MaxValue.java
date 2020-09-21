651a33e3283011661f776f38e341a3a7b253b0f9
wjinshui <ymkscom@gmail.com>
2020-09-21 15:13:30 +0800

    Add ppts

--- Software/src/class3/MaxValue.java
+++ Software/src/class3/MaxValue.java
@@ -1,31 +1,30 @@
 package class3;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
+public class MaxValue
+{
 
-
-public class MaxValue {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		int num = 100;
 		double[] scores = new double[num];
 		Scanner scanner = new Scanner(System.in);
-		Random random = new Random();		
-		for (int i = 0,j =0; i < scores.length; i++) {			
+		Random random = new Random();
+		for (int i = 0; i < scores.length; i++)
+		{
 			scores[i] = random.nextDouble() * 100;
-		}
-		
-		for (double i : scores) {
+		}		
+		Arrays.sort(scores);
+		for (double i : scores)
+		{
 			System.out.println(i);
 		}
 		
-		double max = scores[0];
-		for (int i = 0; i < scores.length; i++) {
-			if(scores[i] > max)
-				max= scores[i];
-		}
-		System.out.println(max);
+		System.out.println(scores[num-1]);
+		System.out.println(scores[0]);
 
 	}
 
